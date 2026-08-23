3b4fee53582df5d0a9310e00bd277cca9477208b
Paul J.L.R. LaRocque P.Eng.  #10185 <paul.larocque@brownrook.com>
2026-08-23 17:54:50 -0500

    test: make numeric receipt selectors authoritative

--- tests/test_receipt_enrichment.py
+++ tests/test_receipt_enrichment.py
@@ -3,8 +3,8 @@ import pytest
 from home_budget_pipeline.receipt_enrichment import receipt_patterns
 
 
-def test_numeric_receipt_selector_keeps_filename_fallback_patterns():
-    assert receipt_patterns("1") == ("%receipt_0001.pdf%", "%receipt_1.pdf%")
+def test_numeric_receipt_selector_does_not_fall_back_to_filename():
+    assert receipt_patterns("1") == ()
 
 
 def test_receipt_selector_accepts_filename_or_path():
